6ebea93e9acbb6e5ccd17b3e81eff16fa8b48294
Klaus Stocker <klaus.stocker@micro-epsilon.at>
2026-04-28 08:38:26 +0200

    change: static method example returns list of examples

--- shared/question_config.py
+++ shared/question_config.py
@@ -12,10 +12,10 @@ class QuestionConfigDto(BaseModel):
     validation: str = ""
     files: dict[str, str] = {}
     evalConfig: EvalConfigDto = EvalConfigDto()
-    
+
     @staticmethod
-    def example():
-        return QuestionConfigDto(
+    def examples() ->list[QuestionConfigDto]:
+        return [QuestionConfigDto(
         indication="""
 def calculate_sum(a, b):
     print('the sum is ' + str(a + b))
@@ -30,10 +30,10 @@ def correctImplementation(arg1, arg2):
 class Checker(unittest.TestCase): # do not rename
     def test_return(self): # names must start with 'test_'
         args = (1, 2)
-        student_result = calculate_sum(*args) # call to students implementation 
+        student_result = calculate_sum(*args) # call to students implementation
         expected_result = correctImplementation(*args)
         self.assertEqual(student_result, expected_result)
-        
+
     def test_output(self):
         args = (3, 4)
         with RedirectedStdout() as student_out:
@@ -42,4 +42,4 @@ class Checker(unittest.TestCase): # do not rename
             correctImplementation(*args)
         self.assertEqual(str(student_out), str(expected_out))
 """
-    )
+    )]
