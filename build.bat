e7b5c6b88d73a05d91e82e1a228c4f098c5cd8f7
klausstocker <klaus.stocker@gmx.net>
2026-05-30 00:53:12 +0200

    Avoid git rev-parse build hash errors

--- build.bat
+++ build.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal
 set "PLUGIN_BUILD_HASH="
-for /f "delims=" %%i in ('git rev-parse --verify --short=12 HEAD 2^>nul') do set "PLUGIN_BUILD_HASH=%%i"
-if not defined PLUGIN_BUILD_HASH if exist revision.txt set /p PLUGIN_BUILD_HASH=<revision.txt
+set "SCRIPT_DIR=%~dp0"
+if exist "%SCRIPT_DIR%.git" (
+    for /f "usebackq delims=" %%i in (`git -C "%SCRIPT_DIR%." log -1 --format^=%%h 2^>nul`) do set "PLUGIN_BUILD_HASH=%%i"
+)
+if not defined PLUGIN_BUILD_HASH if exist "%SCRIPT_DIR%revision.txt" set /p PLUGIN_BUILD_HASH=<"%SCRIPT_DIR%revision.txt"
 if not defined PLUGIN_BUILD_HASH set "PLUGIN_BUILD_HASH=unknown"
 echo Building letto-plugin-python with PLUGIN_BUILD_HASH=%PLUGIN_BUILD_HASH%
-docker build --build-arg "PLUGIN_BUILD_HASH=%PLUGIN_BUILD_HASH%" -t lettohub/letto-plugin-python:latest -f Dockerfile .
+docker build --build-arg "PLUGIN_BUILD_HASH=%PLUGIN_BUILD_HASH%" -t lettohub/letto-plugin-python:latest -f "%SCRIPT_DIR%Dockerfile" "%SCRIPT_DIR%."
 docker compose -f /opt/letto/docker/compose/letto/docker-service-pluginpython.yml down
-copy yml\docker-service-pluginpython.yml \opt\letto\docker\compose\letto\
+copy "%SCRIPT_DIR%yml\docker-service-pluginpython.yml" \opt\letto\docker\compose\letto\
 docker compose -f /opt/letto/docker/compose/letto/docker-service-pluginpython.yml up -d
