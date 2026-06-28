efea02f257f0bcebfa22d1772505e27a8ea42111
klausstocker <klaus.stocker@gmx.net>
2026-06-28 14:21:18 +0200

    Add unittest-backed example workspace

---
+++ examples/calculate_sum/answer.py
@@ -0,0 +1,7 @@
+"""Reference solution for the calculate_sum example."""
+
+
+def calculate_sum(a, b):
+    """Print and return the sum of two values."""
+    print("the sum is " + str(a + b))
+    return a + b
